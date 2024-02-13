5fb9f39458ee7059a03cce1ef6a8a0ecf6d88198
Tristen Smith <38503531+TristenS27@users.noreply.github.com>
2024-02-13 11:33:54 -0500

    Update ProsperLoanData.sql

--- Prosper_Loan_Data/ProsperLoanData.sql
+++ Prosper_Loan_Data/ProsperLoanData.sql
@@ -1,18 +1,17 @@
 -- *Track loan book performance
 -- *Disbursements, amount paid MOM, returns, debt to income ratio, loss rate
 
--- *Change date origination date column from text to data type
--- alter table prosper_loan_data.prosperloandata
--- modify LoanOriginationDate date;
+-- *Change date origination date column from text to date datatype. Comment out after done
+ALTER TABLE prosper_loan_data.prosperloandata MODIFY LoanOriginationDate date
 
 SELECT 
 	-- LoanOriginationDate,
 	date_format(LoanOriginationDate, '%y-%m-01 00:00:00') as month,
-    date_format(LoanOriginationDate, '%y') as year,
+        date_format(LoanOriginationDate, '%y') as year,
 	sum(LoanOriginalAmount) as total_dis,
 	round(sum(LP_CustomerPayments),0) as total_paid,
 	round(avg(EstimatedReturn),2) as profit,
-    round(avg(DebtToIncomeRatio),2) as debtincome,
+        round(avg(DebtToIncomeRatio),2) as debtincome,
 	round(avg(EstimatedLoss),2) as lossrate
     
 -- *Some metrics are only viable from around July 2009  
